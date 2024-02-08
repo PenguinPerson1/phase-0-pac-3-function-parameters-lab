a8c0e069b33dfa052fc29a8ea09d0e77b7a7e0df
Annania Plossl <nia.plossl@gmail.com>
2024-02-08 07:40:33 -0500

    fixed sentence

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I'm learning to program in ${language}`;
 }
 function introductionWithLanguageOptional(name, language = "JavaScript"){
-    return `Hi, my name is ${name} and I am learning to program in ${language}`;
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
